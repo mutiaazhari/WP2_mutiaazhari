d541dcc0e7210bebf93cc331d554925fec935f31
mutiaazhari <rahmamutiaazhari@gmail.com>
2021-10-12 12:06:47 +0700

    tugas pertemuan 5

--- Matakuliah.php
+++ Matakuliah.php
@@ -3,7 +3,6 @@ class Matakuliah extends CI_Controller
 {
 
     public function index()
-
     {
 
         $this->load->view('view-form-matakuliah');
@@ -11,12 +10,36 @@ class Matakuliah extends CI_Controller
 
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
-        ];
+        $this->form_validation->set_rules(
+            'kode',
+            'Kode Matakuliah',
+            'required|min_length[3]',
+            [
 
-        $this->load->view('view-data-matakuliah', $data);
+                'required' => 'Kode Matakuliah Harus diisi',
+                'min_lenght' => 'Kode terlalu pendek'
+            ]
+        );
+        $this->form_validation->set_rules(
+            'nama',
+            'Nama Matakuliah',
+            'required|min_length[3]',
+            [
+                'required' => 'Nama Matakuliah Harus diisi',
+                'min_lenght' => 'Nama terlalu pendek'
+            ]
+        );
+
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
+            ];
+
+            $this->load->view('view-data-matakuliah', $data);
+        }
     }
 }
